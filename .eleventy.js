8ac6a2b12d22ecd5b91d0fd7944be92d57c207f9
Javier Gonzalez <javier.e.glz@gmail.com>
2022-09-13 18:35:35 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = function (config) {
+  config.setLiquidOptions({
+    dynamicPartials: true,
+  });
+
+  // Static assets to pass through
+  config.addPassthroughCopy('./src/images');
+  config.addPassthroughCopy('./src/public');
+  config.addPassthroughCopy('./src/styles');
+  config.addPassthroughCopy('./src/main.js');
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['html', 'md', 'liquid'],
+    htmlTemplateEngine: 'liquid',
+    dataTemplateEngine: 'liquid',
+    markdownTemplateEngine: 'liquid',
+  };
+};
