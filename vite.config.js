ee45f5feefb6bb53391dea32f5be2ccf851c33c5
javier123454321 <javier.e.glz@gmail.com>
2026-08-03 17:22:20 -0500

    fix: include projects pages in vite multi-page build

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,33 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 const { resolve } = require('path');
 const fs = require('fs');
 
-function generateInputOption() {
-  let input = {
-    404: resolve(__dirname, '_site', '404.html'),
-    links: resolve(__dirname, '_site', 'links', 'index.html'),
-    main: resolve(__dirname, '_site', 'index.html'),
-    blog: resolve(__dirname, '_site', 'blog', 'index.html'),
-  };
+function addSection(input, section) {
+  input[section] = resolve(__dirname, '_site', section, 'index.html');
   const paths = fs
-    .readdirSync('src/blog', { withFileTypes: false })
+    .readdirSync('src/' + section, { withFileTypes: false })
     .filter(function isNotAFileType(dir) {
       return dir.search('.html') === -1;
     });
   paths.forEach((element) => {
     element = element.split('.')[0];
-    input['blog/' + element] = resolve(
+    input[section + '/' + element] = resolve(
       __dirname,
       '_site',
-      'blog',
+      section,
       element,
       'index.html',
     );
   });
+}
+
+function generateInputOption() {
+  let input = {
+    404: resolve(__dirname, '_site', '404.html'),
+    links: resolve(__dirname, '_site', 'links', 'index.html'),
+    main: resolve(__dirname, '_site', 'index.html'),
+  };
+  addSection(input, 'blog');
+  addSection(input, 'projects');
   console.log(input)
   return input;
 }
